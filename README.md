754e5445b0bbc7b76f989cfb03d4b2c9fbe9ea3c
Matthias Gutjahr <mattsches@gmail.com>
2016-01-30 20:49:25 +0100

    Update Keystone.js version and dependencies
    * Update versions in package.json
    * Add README
    * Update about and archive views

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Webseite für RheinMainJS auf Basis von KeystoneJS
 
+## Voraussetzungen
+
+* Node.js
+* MongoDB
+
+## Installation
+
+```
+$ git clone https://github.com/rheinmainjs/rmjs-keystone.git
+$ cd rmjs-keystone
+$ npm install
+```
+
+## Konfiguration
+
+* @TODO Optionen aus .env auslesen
+
+## Starten der Applikation
+
+```
+$ node keystone
+```
--- routes/views/about.js
+++ routes/views/about.js
@@ -1,31 +1,27 @@
 var keystone = require('keystone');
 
-exports = module.exports = function(req, res) {
-	
+exports = module.exports = function (req, res) {
+
 	var view = new keystone.View(req, res),
 		locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
+
 	locals.section = 'about';
 	locals.data = {
 		organizers: []
 	};
 
-	// Load the events
-	view.on('init', function(next) {
+	view.on('init', function (next) {
 
 		keystone.list('User').model.find()
 			.where('isOrganiser', true)
 			.sort('name')
-			.exec(function(err, result) {
+			.exec(function (err, result) {
 				locals.data.organizers = result;
 				next(err);
 			});
 
 	});
 
-	// Render the view
 	view.render('views/about');
-	
+
 };
